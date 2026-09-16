97bedd7c4a0da26530196031dfcef2359e8a4a65
sudiptg <mysubho07@gmail.com>
2026-09-16 19:53:13 +0530

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,1 +1,18 @@
 
+from flask import Flask
+from threading import Thread
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "✅ Telegram Bot is running perfectly on Render!"
+
+def run():
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
